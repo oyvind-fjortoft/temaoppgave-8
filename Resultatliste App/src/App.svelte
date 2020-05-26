586b43114557540d9a096e6e2e6cea3eab1bfa0e
oyvind-fjortoft <56107453+oyvind-fjortoft@users.noreply.github.com>
2020-05-26 09:23:47 +0200

    Inkludert Keep Score!

---
+++ Resultatliste App/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Navbar from "./Navbar.svelte";
+  import Player from "./Player.svelte";
+  import AddPlayer from "./AddPlayer.svelte";
+
+  let players = [
+   // {
+    //  name: "Navn Navnesen",
+   //   points: 50
+    },
+  ];
+
+  const addPlayer = e => {
+    const newPLayer = e.detail;
+    players = [...players, newPLayer];
+  };
+
+  const removePlayer = e => {
+    players = players.filter(player => player.name !== e.detail);
+  };
+</script>
+
+<main>
+  <Navbar />
+  <div class="container">
+    <AddPlayer on:addplayer={addPlayer} />
+    {#if players.length === 0}
+      <p>Vennligst legg til spillere.</p>
+    {:else}
+      {#each players as player}
+        <Player
+          name={player.name}
+          points={player.points}
+          on:removeplayer={removePlayer} />
+      {/each}
+    {/if}
+
+  </div>
+</main>
---
+++ Resultatliste App/src/Player.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let name;
+  export let points;
+  let showControls = false;
+
+  const addPoint = () => (points += 1);
+  const removePoint = () => (points -= 1);
+  const toggleControls = () => (showControls = !showControls);
+  const onDelete = () => dispatch("removeplayer", name);
+</script>
+
+<style>
+  h1 {
+    color: #000000;
+    font-weight: 500;
+  }
+
+  h3 {
+    margin-bottom: 10px;
+    font-weight: 400;
+  }
+</style>
+
+<main>
+
+  <div class="card">
+    <div class="navn-buttons">
+      <h1>{name}</h1>
+      <div class="opp-ned-slett">
+        <button class="btn-opp-ned" on:click={toggleControls}>
+          {#if showControls}▲{:else}▼{/if}
+        </button>
+        <button class="btn-slett" on:click={onDelete}>✕</button>
+      </div>
+    </div>
+    <div class="poeng">
+      <h3>Poeng: {points}</h3>
+      {#if showControls}
+        <button class="btn-minus-en" on:click={removePoint}>-1</button>
+        <button class="btn-pluss-en" on:click={addPoint}>+1</button>
+        <input type="number" bind:value={points} />
+      {/if}
+    </div>
+  </div>
+
+</main>
